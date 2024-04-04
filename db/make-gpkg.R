8d3058cb6ca1801d7d2db863fe102917057286d0
whmacken <will.mackenzie@gov.bc.ca>
2024-04-04 13:41:43 -0700

    some notes and change to overwrite

--- db/make-gpkg.R
+++ db/make-gpkg.R
@@ -1,9 +1,11 @@
+### this creates the file with all of the required datatables for functioning on the shiny servers
+
 source("creds.R") # username, password for pg
 pgCon <- DBI::dbConnect(drv = RPostgres::Postgres(), dbname = 'ava_canada', 
   host = Sys.getenv('PG_SM_HOST'), user = username, password = password)
 gpkg <- 'app/becplots/ava_canada.gpkg'
 for (pgTable in DBI::dbListTables(pgCon)) {
   sf::st_read(dsn = pgCon, layer = pgTable) |> 
-    sf::st_write(dsn = gpkg, layer = pgTable)
+    sf::st_write(dsn = gpkg, layer = pgTable, append = FALSE)
 }
 sf::st_layers(gpkg)
