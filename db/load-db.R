11a11caebc824254c70d93a261eb32bdcf814e15
whmacken <will.mackenzie@gov.bc.ca>
2024-04-04 13:41:23 -0700

    Changed the source of data and some of the looup tables

--- db/load-db.R
+++ db/load-db.R
@@ -14,7 +14,7 @@ copy_table <- function(accCon, pgCon, name, prefix = '') {
   dataTable <- DBI::dbReadTable(conn = accCon, name = sprintf('%s%s', prefix, 
     name))
   names(dataTable) <- convert_snake_case(names(dataTable))
-  if (prefix == 'AVA_Canada_Master_') { ### name of access project tables
+  if (prefix == 'AVACanada2024_') { ### name of access project tables
     prefix <- 'ava_canada_' ## name of postgres db tables
   }
   DBI::dbAppendTable(conn = pgCon, name = DBI::SQL(sprintf('%s%s', prefix, name)), 
@@ -36,23 +36,30 @@ dbExecute(pgCon, "DELETE FROM ava_canada_metadata")
 
 # Append Data from Vpro to postgres Tables ------------------------------------------------------
 
-accessPath <- "./data/AVA_Canada_Master.accdb"
+accessPath <- "E:/Arctic/Vpro64_Data_Arctic/AVACanada2024.accdb"
 
 accCon <- dbConnect(odbc::odbc(), .connection_string = 
     sprintf("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=%s;",
       accessPath))
 lapply(dataTables, copy_table, accCon = accCon, pgCon = pgCon,
-  prefix = 'AVA_Canada_Master_')
+  prefix = 'AVACanada2024_')
 
 # Append Lookup Table -----------------------------------------------------
 
-accessPath <- "data/VPro64-23Feb2023/VPro64/VPro64.accdb"
-accCon <- dbConnect(odbc::odbc(), .connection_string = 
-    sprintf("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=%s;",
-      accessPath))
-lapply(lookupTables, copy_table, accCon = accCon, pgCon = pgCon,
-  prefix = '')
+# accessPath <- "E:/Arctic/Vpro64_Arctic/VPro64.accdb"
+# accCon <- dbConnect(odbc::odbc(), .connection_string =
+#     sprintf("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=%s;",
+#       accessPath))
+# lapply(lookupTables, copy_table, accCon = accCon, pgCon = pgCon,
+#   prefix = '')
 
+
+accessPath <- "data/VPro64-23Feb2023/VPro64/VPro64.accdb"
+accCon <- dbConnect(odbc::odbc(), .connection_string =
+                      sprintf("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=%s;",
+                              accessPath))
+lapply(lookupTables, copy_table, accCon = accCon, pgCon = pgCon,
+       prefix = '')
 # Write Zones Table -------------------------------------------------------
 
 zones <- data.table::fread('data/BC_BGCs_Info_v12_15.csv')
@@ -61,42 +68,56 @@ DBI::dbWriteTable(pgCon, name = DBI::SQL('becmaster_zones'), value = zones,
   overwrite = TRUE)
 
 # Write Pub Refs ----------------------------------------------------------
+# 
+# accessPath <- "data/tbl_BEC_References.accdb"
+# accCon <- dbConnect(odbc::odbc(), .connection_string =
+#     sprintf("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=%s;",
+#       accessPath))
+# references <- DBI::dbReadTable(accCon, name = 'tblBEC_References')
+# names(references)[1] <- 'id'
+# names(references) <- tolower(names(references))
+# DBI::dbAppendTable(pgCon, name = DBI::SQL('ava_canada_references'),
+#   value = references)
+# pubs <- data.table::fread('data/AllPublished_LMH_SU.csv')
+# names(pubs) <- c('plot_number', 'site_unit', 'ref_short_name')
+# Encoding(pubs$site_unit)
+# pubs$site_unit <- iconv(pubs$site_unit, "latin1", "UTF-8")
+# DBI::dbWriteTable(pgCon, name = DBI::SQL('ava_canada_link_refs_plots'),
+#   value = pubs, overwrite = TRUE)
 
-accessPath <- "data/tbl_BEC_References.accdb"
-accCon <- dbConnect(odbc::odbc(), .connection_string =
-    sprintf("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=%s;",
-      accessPath))
-references <- DBI::dbReadTable(accCon, name = 'tblBEC_References')
-names(references)[1] <- 'id'
-names(references) <- tolower(names(references))
-DBI::dbAppendTable(pgCon, name = DBI::SQL('ava_canada_references'),
-  value = references)
-pubs <- data.table::fread('data/AllPublished_LMH_SU.csv')
-names(pubs) <- c('plot_number', 'site_unit', 'ref_short_name')
-Encoding(pubs$site_unit)
-pubs$site_unit <- iconv(pubs$site_unit, "latin1", "UTF-8")
-DBI::dbWriteTable(pgCon, name = DBI::SQL('ava_canada_link_refs_plots'),
-  value = pubs, overwrite = TRUE)
 
+# Add Arctic CAVM ------------------------------------------------------
 
-# Add Arctic Regions ------------------------------------------------------
-
-bgcs <- sf::st_read('data/cavm_canada.gpkg') |> 
-  sf::st_transform(4326) |>  st_zm(bgcs)
-names(bgcs) <- tolower(names(bgcs))
-leaflet::leaflet(bgcs) |> 
+cavm <- sf::st_read('data/cavm_canada.gpkg') |> 
+  sf::st_transform(4326) |>  st_zm(cavm)
+names(cavm) <- tolower(names(cavm))
+leaflet::leaflet(cavm) |> 
   leaflet::addTiles() |> 
   leaflet::addPolygons()
-DBI::dbWriteTable(pgCon, value = bgcs, name = DBI::SQL('arctic_bgcs'),
+DBI::dbWriteTable(pgCon, value = cavm, name = DBI::SQL('CAVM'),
   overwrite = TRUE)
 
+# Add CASBEC BGC-----------------------------------------------------------
+# bgc <- sf::st_read('data/casbec_canada.gpkg') |> 
+#   sf::st_transform(4326) |>  st_zm(bgc)
+# names(bgc) <- tolower(names(bgc))
+# leaflet::leaflet(bgc) |> 
+#   leaflet::addTiles() |> 
+#   leaflet::addPolygons()
+# DBI::dbWriteTable(pgCon, value = bgc, name = DBI::SQL('CASBEC'),
+#                   overwrite = TRUE)
 
 # Provinces ---------------------------------------------------------------
 
-provinces <- sf::st_read('data/canadian_provinces_wgs84.gpkg')
+provinces <- sf::st_read('data/canadian_provinces.gpkg')
 names(provinces) <- tolower(names(provinces))
-DBI::dbWriteTable(pgCon, value = provinces, name = DBI::SQL('canada_provinces'),
+DBI::dbWriteTable(pgCon, value = provinces, name = DBI::SQL('Provinces'),
   overwrite = TRUE)
+
+fg_areas <- sf::st_read('data/fieldguide_areas.gpkg')
+names(fg_areas) <- tolower(names(fg_areas))
+DBI::dbWriteTable(pgCon, value = fg_areas, name = DBI::SQL('Fieldguides'),
+                  overwrite = TRUE)
 
 # Grant Privileges --------------------------------------------------------
 
@@ -107,3 +128,4 @@ DBI::dbWriteTable(pgCon, value = provinces, name = DBI::SQL('canada_provinces'),
 
 dbDisconnect(accCon)
 dbDisconnect(pgCon)
+
